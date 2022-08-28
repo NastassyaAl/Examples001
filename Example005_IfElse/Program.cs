f338a5aa4d70780f5ca5f56d7ec2151496aec5c1
unknown <nastena-alhovik@yandex.ru>
2022-08-28 19:39:00 +0300

    add Example005 If Else

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "настя")
+{
+    Console.WriteLine("Ура!!! Это же Настя!!!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
