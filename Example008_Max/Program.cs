f9f4b8997a13fafb4160bc93b8c534e0ad8edcfe
unknown <nastena-alhovik@yandex.ru>
2022-08-31 21:22:20 +0300

    add Example008 Max

---
+++ Example008_Max/Program.cs
@@ -0,0 +1,26 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 12;
+int b1 = 47;
+int c1 = 85;
+int a2 = 2;
+int b2 = 45;
+int c2 = 53;
+int a3 = 4;
+int b3 = 1;
+int c3 = 6780;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
