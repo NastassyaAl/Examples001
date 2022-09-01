9d1aff47acb49722f888bfb02ad17b71ffe166ed
unknown <nastena-alhovik@yandex.ru>
2022-09-01 22:25:35 +0300

    add Example010 Pos

---
+++ Example010_Pos/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 28, 3, 14, 5, 65, 7, 8 };
+
+int n = array.Length;
+
+int find = 4;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
